a4365205989e1e9ec8aeca07681acb1a3a8b71e6
齋藤正浩 <masahirosaito@MacBook-Pro.local>
2020-06-17 20:56:35 +0900

    number of contribute in quota

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
-function myFunction() {
-  console.log('mercari');
-  console.log('income');
+function onOpen() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet();
+  var entries = [
+    {
+      name : "Qiita ユーザー コントリビュート数取得",
+      functionName : "myFunction"
+    }
+  ];
+  sheet.addMenu("スクリプト実行", entries);
+  //メインメニュー部分に[スクリプト実行]メニューを作成して、
+  //下位項目のメニューを設定している
+};
+
+function myFunction () {
+  Browser.msgBox("確認", "Hello GAS World.", Browser.Buttons.OK);
 }
