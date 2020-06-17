875e41e584c1de3efee49c55fd3a8c1d499c6096
齋藤正浩 <masahirosaito@MacBook-Pro.local>
2020-06-17 21:43:33 +0900

    acquire of web data

--- code.js
+++ code.js
@@ -1,16 +1,30 @@
-function onOpen() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet();
-  var entries = [
-    {
-      name : "Qiita ユーザー コントリビュート数取得",
-      functionName : "myFunction"
-    }
-  ];
-  sheet.addMenu("スクリプト実行", entries);
-  //メインメニュー部分に[スクリプト実行]メニューを作成して、
-  //下位項目のメニューを設定している
-};
-
 function myFunction () {
-  Browser.msgBox("確認", "Hello GAS World.", Browser.Buttons.OK);
+  var book = SpreadsheetApp.getActiveSpreadsheet();
+  var sheetData = book.getSheetByName("シート1");
+
+  var colID = 1;
+  var colURL = 2;
+  var colContributeCount = 3;
+  var colIcon = 4;
+
+  var rowStartData = 2
+  var rowEndData = sheetData.getDataRange().getLastRow()
+
+  for (var i = rowStartData; i <= rowEndData; i += 1) {
+      var url = 'http://qiita.com/' + sheetData.getRange(i, colID).getValue();
+      sheetData.getRange(i, colURL).setValue(url);
+      var response = UrlFetchApp.fetch(url);
+      var html = response.getContentText('UTF-8');
+
+      var searchTag = '/contributions"><span class="userActivityChart_statCount">';
+      var index = html.indexOf(searchTag)
+      if (index !== -1) {
+        var html = html.substring(index + searchTag.length);
+        var index = html.indexOf('</span>');
+        if (index !== -1) {
+          sheetData.getRange(i, colContributeCount).setValue(html.substring(0, index));
+        }
+      }
+
+  }
 }
